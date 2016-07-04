48b55f777f025533d7f9c75f11316a56ce717b7a
EC2 Default User <ec2-user@ip-172-31-60-114.ec2.internal>
2016-07-04 09:17:56 +0000

    add create dir

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -4,6 +4,7 @@ cp $KEY_PATH ~/.ssh/id_rsa
 chmod -R 0400 ~/.ssh/
 ssh-keyscan -p 722 $GIT_HOST >> ~/.ssh/known_hosts
 array=($GIT_REPOS)
+mkdir -p ${GIT_BASE_PATH}/.niogit/
 cd ${GIT_BASE_PATH}/.niogit/
 for repo_name in "${array[@]}";
 do
