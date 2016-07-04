d87078cfdadd54ce97e83e0d191495a6180c0ec6
EC2 Default User <ec2-user@ip-172-31-60-114.ec2.internal>
2016-07-04 08:59:27 +0000

    first commit

---
+++ scripts/restore.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p  ~/.ssh/
+cp $KEY_PATH ~/.ssh/id_rsa
+chmod -R 0400 ~/.ssh/
+ssh-keyscan -p 722 $GIT_HOST >> ~/.ssh/known_hosts
+array=($GIT_REPOS)
+cd ${GIT_BASE_PATH}/.niogit/
+for repo_name in "${array[@]}";
+do
+git clone ssh://git@${GIT_HOST}:722/drools/${repo_name}.git
+done
